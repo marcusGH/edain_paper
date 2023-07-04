8ce7a56ee11a4ff904d075eb9e014e101d448bd9
= <=>
2023-07-04 21:08:57 +0100

    Extended adaptive preprocesser with moving average mean and std

--- src/preprocessing/adaptive_transformations.py
+++ src/preprocessing/adaptive_transformations.py
@@ -8,7 +8,7 @@ class FullDAIN_Layer(nn.Module):
     def __init__(self, input_dim, time_series_length,
         adaptive_winsorization=True, adaptive_scale=True,
         adaptive_shift=True, adaptive_power_transform=True,
-        eps=1e-8,
+        eps=1e-8, dev=torch.device('cuda'),
     ):
         super(FullDAIN_Layer, self).__init__()
 
@@ -24,13 +24,18 @@ class FullDAIN_Layer(nn.Module):
         self.adaptive_shift = adaptive_shift
         self.adaptive_power_transform = adaptive_power_transform
 
+        # for use in adaptive scale and shift
+        self.running_mean = torch.zeros(self.D, device=dev)
+        self.running_second_moment = torch.zeros(self.D, device=dev)
+        self.running_n = torch.tensor(0, device=dev)
+
         # for adaptive winsorization
         self.alpha = nn.Parameter(
-            data=torch.Tensor(1, self.D, self.T),
+            data=torch.Tensor(1, self.T, self.D),
             requires_grad=True,
         )
         self.beta = nn.Parameter(
-            data=torch.Tensor(1, self.D, self.T),
+            data=torch.Tensor(1, self.T, self.D),
             requires_grad=True,
         )
         # default to tanh in (0, 1)
@@ -41,34 +46,34 @@ class FullDAIN_Layer(nn.Module):
         self.shift_layer = nn.Linear(self.D, self.D, bias=False)
         self.shift_layer.weight.data = torch.FloatTensor(data=np.eye(self.D, self.D))
         self.skip_shift = nn.Parameter(
-            data=torch.Tensor(1, self.D, self.T),
+            data=torch.Tensor(1, self.T, self.D),
             requires_grad=True,
         )
-        nn.init.constant_(self.skip_shift, 10.)
+        nn.init.constant_(self.skip_shift, 0.)
 
         # for adaptive scaling
         self.scaling_layer = nn.Linear(self.D, self.D, bias=False)
         self.scaling_layer.weight.data = torch.FloatTensor(data=np.eye(self.D, self.D))
         self.skip_scale = nn.Parameter(
-            data=torch.Tensor(1, self.D, self.T),
+            data=torch.Tensor(1, self.T, self.D),
             requires_grad=True,
         )
-        nn.init.constant_(self.skip_scale, 10.)
+        nn.init.constant_(self.skip_scale, 0.)
 
         # for adaptive power-transform
         self.lambd = nn.Parameter(
-            data=torch.Tensor(1, self.D, self.T),
+            data=torch.Tensor(1, self.T, self.D),
             requires_grad=True,
         )
         self.skip_power_transform = nn.Parameter(
-            data=torch.Tensor(1, self.D, self.T),
+            data=torch.Tensor(1, self.T, self.D),
             requires_grad=True,
         )
         nn.init.ones_(self.lambd)
         nn.init.zeros_(self.skip_power_transform)
 
         # for saving power transform forward pass output
-        self.power_transform_out = None # (N, D, T)
+        self.power_transform_out = None # (N, T, D)
         def power_transform_backward_hook(_):
             """
             The default gradient computations for lambda update is very
@@ -77,12 +82,12 @@ class FullDAIN_Layer(nn.Module):
             a KL-divergence loss between the output samples and a normal distribution
             with the same mean and std.
             """
-            x = self.power_transform_out # (N, D, T)
+            x = self.power_transform_out # (N, T, D)
             mean_x = torch.mean(x, 0, keepdim=False)
             std_x = torch.std(x, 0, keepdim=False)
             # create a normal distribution with same moments as the input
             norm = torch.distributions.normal.Normal(loc=mean_x, scale=std_x)
-            # Grads should be of shape (1, D, T)
+            # Grads should be of shape (1, T, D)
             return torch.zeros_like(grad)
         self.lambd.register_hook(power_transform_backward_hook)
 
@@ -91,26 +96,12 @@ class FullDAIN_Layer(nn.Module):
         (1 - alpha') * x + alpha ' * beta' * tanh(x / beta')
          where alpha' in (0, 1) and beta' > 0
         """
-        mean_x = torch.mean(x, 0, keepdim=True) # shape (D, T)
+        mean_x = torch.mean(x, 0, keepdim=True) # shape (T, D)
         # scale to mean 0, then scale back to ensure winsorization symmetric
         winsorized_x = torch.exp(self.beta) * torch.tanh((x - mean_x) * torch.exp(-self.beta)) + mean_x
         # residual bypass
         return torch.sigmoid(self.alpha) * x + (1. - torch.sigmoid(self.alpha)) * winsorized_x
 
-    def _adaptive_shift(self, x):
-        # Input of shape (N, D, T)
-        avg = torch.mean(x, 2) # (N, D)
-        adaptive_avg = self.shift_layer(avg) # (N, D)
-        return x - adaptive_avg.unsqueeze(-1)
-
-    def _adaptive_scale(self, x):
-        # Input of shape (N, D, T)
-        std = torch.mean(x ** 2, 2) # (N, D)
-        std = torch.sqrt(std + self.eps)
-        adaptive_std = self.scaling_layer(std)
-        # to avoid numerical errors
-        adaptive_std[adaptive_std <= self.eps] = 1
-        return x / adaptive_std.unsqueeze(-1)
 
     def _adaptive_power_transform(self, x, lambd):
         """
@@ -150,9 +141,17 @@ class FullDAIN_Layer(nn.Module):
     def forward(self, x, dim_first=True):
         """
         Input of shape (N, D, T) if dim_first=True, otherwise (N, T, D)
+        Internally, we work with (N, T, D)
         """
-        if not dim_first:
+        if dim_first:
             x = x.transpose(1, 2)
+
+        # cumulative running estimates of E[X] and E[X^2], and sqrt(V[X])
+        # this is done over both the temporal and n-axis, to get a result of shape (D,)
+        assert (x.size(1), x.size(2)) == (self.T, self.D)
+        mean_x = (self.running_mean * self.running_n + torch.sum(x.view((-1, self.D)), 0)) / (self.running_n + x.size(0) * x.size(1))
+        mean_x2 = (self.running_second_moment * self.running_n + torch.sum(x.view((-1, self.D)) ** 2, 0)) / (self.running_n + x.size(0) * x.size(1))
+        std = torch.sqrt(mean_x2 - mean_x ** 2 + self.eps)
 
         # Step 1: Winsorization
         if self.adaptive_winsorization:
@@ -160,16 +159,26 @@ class FullDAIN_Layer(nn.Module):
 
         # Step 2: Shift
         if self.adaptive_shift:
-            x = (1. - torch.sigmoid(self.skip_shift)) * self._adaptive_shift(x) + torch.sigmoid(self.skip_shift) * x
+            adaptive_avg = self.shift_layer(mean_x.unsqueeze(0)).unsqueeze(0) # (1, 1, D)
+            x = (1. - torch.sigmoid(self.skip_shift)) * (x - adaptive_avg) + torch.sigmoid(self.skip_shift) * x
 
         # Step 3: Scale
         if self.adaptive_scale:
-            x = (1. - torch.sigmoid(self.skip_scale)) * self._adaptive_scale(x) + torch.sigmoid(self.skip_scale) * x
+            adaptive_std = self.scaling_layer(std.unsqueeze(0)).unsqueeze(0) # (1, 1, D)
+            # to avoid numerical errors
+            adaptive_std[adaptive_std <= self.eps] = 1.0
+            x = (1. - torch.sigmoid(self.skip_scale)) * (x / adaptive_std) + torch.sigmoid(self.skip_scale) * x
 
         # Step 4: Power transform
         if self.adaptive_power_transform:
             x = (1. - torch.sigmoid(self.skip_power_transform)) * self._adaptive_power_transform(x, self.lambd) + torch.sigmoid(self.skip_power_transform) * x
 
-        if not dim_first:
-            x = x.tranpose(1, 2)
+        # update running estimates
+        self.running_mean = mean_x.detach()
+        self.running_second_moment = mean_x2.detach()
+        self.running_n += x.size(0) * x.size(1)
+
+        # flip dimensions back
+        if dim_first:
+            x = x.transpose(1, 2)
         return x
